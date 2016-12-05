ffecbbe9405ff06a9d75dd35dfb2a904e79dc6ad
Linas Valiukas <lvaliukas@cyber.law.harvard.edu>
2016-12-05 17:27:22 +0800

    Remove “j” argument (not really useful either)

--- hausastemmer/HausaStemmer.py
+++ hausastemmer/HausaStemmer.py
@@ -928,7 +928,7 @@ class HausaStemmer:
         if self.__chk_center_combo_3():
             self.b = self.b[self.k0:self.k0 + 2] + self.b[self.k0 + 5:self.k + 1]
 
-    def stem(self, p, j, lookup=True):
+    def stem(self, p, lookup=True):
         """In stem(p,i,j), p is a char pointer, and the string to be stemmed is from p[i] to p[j] inclusive. Typically i
         is zero and j is the offset to the last character of a string, (p[j+1] == '\0'). The stemmer adjusts the
         characters p[i] ... p[j] and returns the new end-point of the string, k. Stemming never increases word length,
@@ -940,7 +940,7 @@ class HausaStemmer:
 
         # copy the parameters into statics
         self.b = p
-        self.k = j
+        self.k = len(p) - 1
         self.j = self.k
         self.k0 = 0
         if self.k <= self.k0 + 1:
