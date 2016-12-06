60ec268ea714f2e16f2127e85d8002f32de8051c
Linas Valiukas <lvaliukas@cyber.law.harvard.edu>
2016-12-07 01:09:37 +0800

    Add Pydoc on top of __init__.py for `pydoc` to work

--- hausastemmer/__init__.py
+++ hausastemmer/__init__.py
@@ -1,3 +1,5 @@
+# coding=utf-8
+
 # Copyright (C) 2013 Bimba Andrew Thomas, 2016 Linas Valiukas
 #
 # This program is free software: you can redistribute it and/or modify
@@ -10,6 +12,22 @@
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details <http://www.gnu.org/licenses/>.
 
+
+"""
+Hausa language stemmer reference implementation by Bimba et al., 2015. Uses Hausa affix-rules
+and reference lookup to stem words in Hausa language.
+
+Paper: Bimba, A., Idris, N., Khamis, N. and Noor, N.F.M. (2015) ‘Stemming Hausa text: Using affix-stripping rules and
+reference look-up’, Language Resources and Evaluation, 50(3), pp. 687–703. doi: 10.1007/s10579-015-9311-x.
+URL: https://bit.ly/hausa-stemming-bimba.
+
+Based on modifications to the implementation of the Porter's Stemming Algorithm by Vivake Gupta (2001).
+
+Copyright (C) 2013 Bimba Andrew Thomas <andrewbimba@gmail.com>, Department of Artificial Intelligence,
+FSKTM Universiti Malaya, Kuala Lumpur, Malaysia.
+
+Adapted as a Python module by Linas Valiukas <lvaliukas@cyber.law.harvard.edu>.
+"""
 
 from hausastemmer.HausaStemmer import HausaStemmer
 import hausastemmer.__about__
